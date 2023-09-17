2c7e251ffd734be448d853bdd67bf245cf600ccb
Muhammad Usama <usama@Muhammads-MacBook-Pro.local>
2023-09-17 16:30:40 +0500

    cors enabled

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const server = app.listen(port, console.log("Hello world"));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
   },
 });
 
